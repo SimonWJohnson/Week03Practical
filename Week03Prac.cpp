54527a89dcc3c8df63f5f9891c04e6efb2832c93
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-27 00:22:42 +0930

    completed!  Added a while loop to handle prompting the user to chooses again after an invalid input

--- Week03Prac.cpp
+++ Week03Prac.cpp
@@ -8,14 +8,44 @@
 
 int main()
 {
-    // quick local variable test
-    ApplePie apple;
-    RaspberryPie raspberry;
+    int selection = 0;
+    Pie* piePtr = nullptr;
 
-    std::cout << apple.description() << std::endl;
-    std::cout << raspberry.description() << std::endl;
+    while (piePtr == nullptr) 
+    {
+        std::cout << "Choose a pie:" << std::endl;
+        std::cout << "1. Apple Pie" << std::endl;
+        std::cout << "2. Rapsberry Pie" << std::endl;
+        std::cout << "3. Apricot Pie" << std::endl;
+        std::cout << "Enter choice: " << std::endl;
+        std::cin >> selection;
 
+        if (selection == 1)
+        {
+            piePtr = new ApplePie();
+        }
+        else if (selection == 2)
+        {
+            piePtr = new RaspberryPie();
+        }
+        else if (selection == 3)
+        {
+            piePtr = new ApricotPie();
+        }
+        else
+        {
+            std::cout << "Invalid selection" << std::endl;
+            //return 0;
+            continue;
+        }
+    }
+
+    std::cout << "You chose: " << piePtr->description() << std::endl;
+
+    delete piePtr;
+    piePtr = nullptr;
     return 0;
+
 }
 
 
